a2b8c0601f79e6bac4547833574bc3459f3f305b
Dominik <aeduel@gmail.com>
2019-08-22 12:54:44 +0200

    Update OfertyZapytanie.sql

--- Zapytania SQL/OfertyZapytanie.sql
+++ Zapytania SQL/OfertyZapytanie.sql
@@ -6,17 +6,17 @@ CDN.OfertyDok.OfD_Nazwa AS 'Nazwa',
 CDN.OfertyDok.OfD_Opis AS 'Opis',
 CDN.DefEtapy.DEt_Kod AS 'Status',
 CDN.OfertyDok.OfD_StatusOpis AS 'Opis Statusu',
-CDN.OfertyDok.OfD_OpeZalKod AS 'Kto Za³o¿y³',
-CDN.OfertyDok.OfD_OpeModKod AS 'Kto Ostatnio Modyfikowa³',
+CDN.OfertyDok.OfD_OpeZalKod AS 'Kto ZaÂ³oÂ¿yÂ³',
+CDN.OfertyDok.OfD_OpeModKod AS 'Kto Ostatnio ModyfikowaÂ³',
 CDN.OfertyKnt.OfK_Kod AS 'Kontrahent',
-CDN.OfertyDok.OfD_DataOd AS 'Obowi¹zuje Od',
-CDN.OfertyDok.OfD_DataDo AS 'Obowi¹zuje Do',
+CDN.OfertyDok.OfD_DataOd AS 'ObowiÂ¹zuje Od',
+CDN.OfertyDok.OfD_DataDo AS 'ObowiÂ¹zuje Do',
 
 (SELECT SUM(CDN.OfertyElem.OfC_WartoscNetto) FROM CDN.OfertyElem WHERE CDN.OfertyElem.OfL_OfDId = CDN.OfertyDok.OfD_OfDId) AS 'Kwota Netto',
 
 
 CDN.OfertyDok.OfD_Waluta AS 'Waluta',
---Na Procenty trzeba coœ wymyœliæ
+--Na Procenty trzeba coÅ“ wymyÅ“liÃ¦
 
 CDN.OfertyDok.OfD_NumerPelny AS 'numer Dokumentu',
 
@@ -27,7 +27,7 @@ CASE
 	END AS 'Czy w Buforze'
 
 FROM CDN.OfertyDok
-	JOIN CDN.OfertyKnt ON CDN.OfertyDok.OfD_OfDId = CDN.OfertyKnt.OfK_OfDId
-	JOIN CDN.DefEtapy ON CDN.OfertyDok.OfD_StatusId = CDN.DefEtapy.DEt_DEtId
+	LEFT OUTER JOIN CDN.OfertyKnt ON CDN.OfertyDok.OfD_OfDId = CDN.OfertyKnt.OfK_OfDId
+	LEFT OUTER JOIN CDN.DefEtapy ON CDN.OfertyDok.OfD_StatusId = CDN.DefEtapy.DEt_DEtId
 
 WHERE OfD_TS_Zal > CAST(YEAR(getdate()) AS varchar(4)) + '-01-01 00:00:00.000';
