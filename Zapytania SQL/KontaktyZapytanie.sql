0ea8661e7d39f1841fa58529c8398b80ca6d9a4d
Dominik <aeduel@gmail.com>
2019-08-22 12:54:12 +0200

    Update KontaktyZapytanie.sql

--- Zapytania SQL/KontaktyZapytanie.sql
+++ Zapytania SQL/KontaktyZapytanie.sql
@@ -12,20 +12,20 @@ SELECT
 
 	CASE
 		WHEN CDN.CRMKontakty.CRK_Obsluga = 0
-			THEN 'Przed Sprzeda¿¹'
-			ELSE 'Po Sprzeda¿y'
+			THEN 'Przed SprzedaÂ¿Â¹'
+			ELSE 'Po SprzedaÂ¿y'
 	END AS 'Obsluga',
 
-	CDN.CRMKontakty.CRK_OpeZalKod AS 'Kto Za³o¿y³',
-	CDN.CRMKontakty.CRK_OpeModKod AS 'Kto Ostatnio Zmodyfikowa³',
+	CDN.CRMKontakty.CRK_OpeZalKod AS 'Kto ZaÂ³oÂ¿yÂ³',
+	CDN.CRMKontakty.CRK_OpeModKod AS 'Kto Ostatnio ZmodyfikowaÂ³',
 
 	CASE
 		WHEN CDN.CRMKontakty.CRK_Bufor = 0
-			THEN 'Zakoñczono'
+			THEN 'ZakoÃ±czono'
 			ELSE 'Kontakt W Realizacji'
 	END AS 'Bufor',
 
-	CDN.CRMKontakty.CRK_TS_Zal AS 'Data Za³o¿enia Dokumentu',
+	CDN.CRMKontakty.CRK_TS_Zal AS 'Data ZaÂ³oÂ¿enia Dokumentu',
 
 	CASE
 		WHEN CDN.CRMKontakty.CRK_Zadanie = 0
@@ -37,23 +37,23 @@ SELECT
 	CDN.CRMKontakty.CRK_TerminDo AS 'Termin Do',
 
 	CASE 
-		WHEN CDN.CRMKontakty.CRK_Zadanie = 1 AND CDN.CRMKontakty.CRK_Priorytet = 1 THEN 'Najwy¿szy' 
+		WHEN CDN.CRMKontakty.CRK_Zadanie = 1 AND CDN.CRMKontakty.CRK_Priorytet = 1 THEN 'NajwyÂ¿szy' 
 		WHEN CDN.CRMKontakty.CRK_Zadanie = 1 AND CDN.CRMKontakty.CRK_Priorytet = 2 THEN 'Wysoki'
 		WHEN CDN.CRMKontakty.CRK_Zadanie = 1 AND CDN.CRMKontakty.CRK_Priorytet = 3 THEN 'Niski'
-		WHEN CDN.CRMKontakty.CRK_Zadanie = 1 AND CDN.CRMKontakty.CRK_Priorytet = 4 THEN 'Najni¿szy'
+		WHEN CDN.CRMKontakty.CRK_Zadanie = 1 AND CDN.CRMKontakty.CRK_Priorytet = 4 THEN 'NajniÂ¿szy'
 		ELSE '' 
 	END AS Priorytet,
 
 	CAST((CDN.CRMKontakty.CRK_CzasKontaktu-'1899-12-30 00:00:00.000') as time(0)) AS 'Czas Kontaktu',
-	CDN.CRMKontakty.CRK_DataDok AS 'Data Rozpoczêcia'
+	CDN.CRMKontakty.CRK_DataDok AS 'Data RozpoczÃªcia'
 	
 
 FROM 
-	CDN.CRMKontakty JOIN 
-				CDN.operatorzy ON CDN.CRMKontakty.CRK_OpiekunId = CDN.operatorzy.Ope_OpeID
-			JOIN 
+	CDN.CRMKontakty LEFT OUTER JOIN 
+				CDN_KNF_Konfiguracjaf.cdn.Operatorzy ON = Ope_OpeID = CDN.CRMKontakty.CRK_OpiekunId 
+			LEFT OUTER JOIN  
 				CDN.Kontrahenci ON CDN.CRMKontakty.CRK_PodID = CDN.Kontrahenci.Knt_KntId
-			JOIN
+			LEFT OUTER JOIN 
 				CDN.DefEtapy ON CDN.CRMKontakty.CRK_EtapRealizacji = CDN.DefEtapy.DEt_DEtId
 WHERE 
 	CRK_TS_Zal > CAST(YEAR(getdate()) AS varchar(4)) + '-01-01 00:00:00.000';
