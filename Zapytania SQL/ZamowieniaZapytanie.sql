ae4c9ce80313217f95f08135f82492d5ef4e9ee2
Dominik <aeduel@gmail.com>
2019-08-22 12:56:30 +0200

    Update ZamowieniaZapytanie.sql

--- Zapytania SQL/ZamowieniaZapytanie.sql
+++ Zapytania SQL/ZamowieniaZapytanie.sql
@@ -1,27 +1,28 @@
-***ZAMÓWIENIA***
+***ZAMÃ“WIENIA***
 
 
 USE CDN3;
 SELECT
 
 CDN.TraNag.TrN_Opis AS 'Opis',
-CDN.TraNag.TrN_OpeZalKod AS 'Kto Za³o¿y³',
-CDN.TraNag.TrN_OpeModKod AS 'Kto Ostatnio Zmodyfikowa³',
+CDN.TraNag.TrN_OpeZalKod AS 'Kto ZaÂ³oÂ¿yÂ³',
+CDN.TraNag.TrN_OpeModKod AS 'Kto Ostatnio ZmodyfikowaÂ³',
 CDN.Kontrahenci.Knt_Kod AS 'Kontrahent',
 CDN.operatorzy.Ope_Kod AS 'Opiekun',
 CDN.TraNag.TrN_RazemNetto AS 'Kwota Netto',
 CDN.TraNag.TrN_DataWys AS 'Data Wystawienia',
 CDN.TraNag.TrN_Rabat AS 'Rabat',
-CDN.FormyPlatnosci.FPl_Nazwa AS 'P³atnoœæ', 
+CDN.FormyPlatnosci.FPl_Nazwa AS 'PÂ³atnoÅ“Ã¦', 
 CDN.TraNag.TrN_Termin AS 'Termin',
-CDN.TraNag.TrN_NumerPelny AS 'Numer Zamówienia'
+CDN.TraNag.TrN_NumerPelny AS 'Numer ZamÃ³wienia',
+isnull([cdn].[ProcentWplaconychZaliczek2] (trn_trnid),0) AS 'Procent WpÅ‚aconych'
 
 FROM CDN.TraNag		
-	JOIN 
+	LEFT OUTER JOIN 
 		CDN.Kontrahenci ON CDN.TraNag.TrN_PodID = CDN.Kontrahenci.Knt_KntId
-	JOIN 
-		CDN.operatorzy ON CDN.Kontrahenci.Knt_OpiekunId = CDN.operatorzy.Ope_OpeID
-	JOIN
+	LEFT OUTER JOIN 
+		CDN_KNF_Konfiguracjaf.cdn.Operatorzy ON Ope_OpeID = CDN.Kontrahenci.Knt_OpiekunId
+	LEFT OUTER JOIN 
 		CDN.FormyPlatnosci ON CDN.TraNag.TrN_FPlId = CDN.FormyPlatnosci.FPl_FPlId
 
 WHERE TrN_NumerPelny LIKE 'ZAM%' + CAST(YEAR(getdate()) AS varchar(4));
