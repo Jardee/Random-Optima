8b29335ea6514e437a72fecffdeea45dc70f4207
Dominik <aeduel@gmail.com>
2019-09-02 09:53:56 +0200

    Update ZleceniaSerwisoweZapytanie.sql

--- Zapytania SQL/ZleceniaSerwisoweZapytanie.sql
+++ Zapytania SQL/ZleceniaSerwisoweZapytanie.sql
@@ -13,18 +13,23 @@ CASE
 		ELSE 'Tak'
 	END AS 'Czy w Buforze',
 CDN.Kontrahenci.Knt_Kod AS 'Kontrahent',
-CDN.SrsZlecenia.SrZ_TS_Zal AS 'Data Za³o¿enia',
-CDN.SrsZlecenia.SrZ_DataPrzyjecia AS 'Data Przyjêcia',
+CDN.SrsZlecenia.SrZ_TS_Zal AS 'Data ZaÅ‚oÅ¼enia',
+CDN.SrsZlecenia.SrZ_DataPrzyjecia AS 'Data PrzyjÄ™cia',
 CDN.SrsZlecenia.SrZ_DataRealizacji AS 'Data Realizacji',
-CDN.SrsZlecenia.SrZ_DataZamkniecia AS 'Data Zamkniêcia',
+CDN.SrsZlecenia.SrZ_DataZamkniecia AS 'Data ZamkniÄ™cia',
 CDN.SrsZlecenia.SrZ_EtapOpis AS 'Etap',
-CDN.SrsZlecenia.SrZ_Stan AS 'Etap',
+CDN.SrsZlecenia.SrZ_Stan AS 'Stan',
 CDN.SrsZlecenia.SrZ_Opis AS 'Opis',
-CDN.SrsZlecenia.SrZ_WartoscNetto AS 'Wartoœæ Netto',
-CAST(CDN.SrsZlecenia.SrZ_CzasRealizacjiCzynnosci / 60 as varchar(10))  + ' h' AS 'Czas Realizacji Czynnoœci',
-CDN.SrsZlecenia.SrZ_OpeZalKod AS 'Kto Za³o¿y³',
-CDN.SrsZlecenia.SrZ_OpeModKod AS 'Kto Ostatnio Modyfikowa³'
+CDN.SrsZlecenia.SrZ_WartoscNetto AS 'WartoÅ›Ä‡ Netto',
+CAST(CDN.SrsZlecenia.SrZ_CzasRealizacjiCzynnosci / 60 as varchar(10))  + ' h' AS 'Czas Realizacji Czynno?ci',
+CDN.SrsZlecenia.SrZ_OpeZalKod AS 'Kto ZaÅ‚oÅ¼yÅ‚',
+CDN.SrsZlecenia.SrZ_OpeModKod AS 'Kto Ostatnio ModyfikowaÅ‚',
+CDN.Kategorie.Kat_KodOgolny AS 'Kod OgÃ³lny Kategorii',
+CDN.Kategorie.Kat_KodSzczegol AS 'Kod SzczegÅ‚owy Kategorii'
+
 FROM CDN.SrsZlecenia 
-	JOIN 
+	LEFT JOIN 
 		CDN.Kontrahenci ON CDN.SrsZlecenia.SrZ_PodmiotId = CDN.Kontrahenci.Knt_KntId
+	LEFT OUTER JOIN  
+		CDN.Kategorie ON CDN.SrsZlecenia.SrZ_KatID = CDN.Kategorie.Kat_KatID
 WHERE CDN.SrsZlecenia.SrZ_TS_Zal > CAST(YEAR(getdate()) AS varchar(4)) + '-01-01 00:00:00.000';
